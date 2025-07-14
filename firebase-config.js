f11e59a7718ca8239b72c155758cb7a6b904c4db
Cata Catha <89225673+Homero2016@users.noreply.github.com>
2025-07-14 02:01:34 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDbz-PJ9OSELTu1tTg2hSPAST8VouWqeEc",
-  authDomain: "malla-medicina-unab.firebaseapp.com",
-  projectId: "malla-medicina-unab",
-  storageBucket: "malla-medicina-unab.firebasestorage.app",
-  messagingSenderId: "624124095109",
-  appId: "1:624124095109:web:83649db993b8f570afd7ec",
-};
