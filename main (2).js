8fa08f10f2aa56dc0e03bbdb83c64bbdd0301a13
Cata Catha <89225673+Homero2016@users.noreply.github.com>
2025-07-17 14:11:35 -0400

    Update main (2).js

--- main (2).js
+++ main (2).js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDbz-PJ9OSELTu1tTg2hSPAST8VouWqeEc",
   authDomain: "malla-medicina-unab.firebaseapp.com",
   projectId: "malla-medicina-unab",
-  storageBucket: "malla-medicina-unab.appspot.com",
+  storageBucket: "malla-medicina-unab.firebasestorage.app",
   messagingSenderId: "624124095109",
   appId: "1:624124095109:web:83649db993b8f570afd7ec",
   measurementId: "G-QR06PHWGZX"
